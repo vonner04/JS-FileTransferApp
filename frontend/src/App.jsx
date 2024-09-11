e7c0fe20e5c43903e0d677d91f4d43c570fc9439
vonner04 <randyleean092m@gmail.com>
2024-09-12 03:21:50 +1200

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,10 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import "./index.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>App</div>
+      <div className="text-4xl">App</div>
       <div>fragment</div>
     </>
   );
