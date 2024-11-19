39db2f547fc5f0fb6e1669245895cfe85ac19f7f
vonner04 <randyleean092m@gmail.com>
2024-11-19 15:58:55 +1300

    feat: setup tailwind and started on navigation bar.

---
+++ front-end/src/components/navigation-bar/Navigation.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+    <div className ="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-base text-primary-text">
+      <i>=</i>
+      <i>Home</i>
+      <i>File Transfer</i>
+      <i>Recent Transfer</i>
+      <i>Your Files</i>
+      <i>Account Settings</i>
+    </div>
+  )
+}
+
+export default Navigation
