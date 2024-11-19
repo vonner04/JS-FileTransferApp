dbb6be6122f8558aa19685694286c3ec2fe4bfb6
vonner04 <randyleean092m@gmail.com>
2024-11-19 20:56:20 +1300

    feat: navbar layout implemented and react-icons installed
    bugfix: colors are now extended instead of replacing default palette.

--- front-end/src/components/navigation-bar/Navigation.jsx
+++ front-end/src/components/navigation-bar/Navigation.jsx
@@ -1,14 +1,29 @@
 import React from 'react'
+import { TfiMenu, TfiHome, TfiShare, TfiTime, TfiFolder, TfiSettings } from "react-icons/tfi";
 
 function Navigation() {
   return (
-    <div className ="fixed top-0 left-0 h-screen w-16 m-0 flex flex-col bg-base text-primary-text">
-      <i>=</i>
-      <i>Home</i>
-      <i>File Transfer</i>
-      <i>Recent Transfer</i>
-      <i>Your Files</i>
-      <i>Account Settings</i>
+    <div className ="fixed top-0 left-0 h-screen w-52 m-0 grid grid-rows-[auto,1fr,auto] items-start bg-primary-background text-primary-text text-xl">
+      {/* Menu Icon */}
+      <div className='flex flex-row justify-center'> 
+      <TfiMenu className="sidebar-icon mt-4 "/>
+      </div>
+      
+      {/* Links*/}
+      <div className='flex flex-col gap-y-8 mt-4'>
+        <div className='flex flex-row items-center space-x-4'>
+          <TfiHome className='sidebar-icon left-1'></TfiHome><span>Home</span></div>
+        <div className='flex flex-row items-center space-x-4'>
+          <TfiShare className='sidebar-icon left-1'></TfiShare><span>File Transfer</span></div>
+        <div className='flex flex-row items-center space-x-4'>
+          <TfiTime className='sidebar-icon left-1'></TfiTime><span>Recent Transfer</span></div>
+        <div className='flex flex-row items-center space-x-4'>
+          <TfiFolder className='sidebar-icon left-1'></TfiFolder><span>My Files</span>
+        </div>
+      </div>
+     
+      <div className='flex flex-row items-center space-x-4 mb-4'>
+        <TfiSettings className='sidebar-icon left-1'></TfiSettings><span>Account Settings</span></div>
     </div>
   )
 }
