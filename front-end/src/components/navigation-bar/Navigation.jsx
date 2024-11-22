3cb67ed8868fba54e54870ec39996e34277424a0
vonner04 <randyleean092m@gmail.com>
2024-11-22 17:12:00 +1300

    fix: content and navigation bar are now side-by-side.
    feat: active link is now highlighted.

--- front-end/src/components/navigation-bar/Navigation.jsx
+++ front-end/src/components/navigation-bar/Navigation.jsx
@@ -4,50 +4,64 @@ import { TfiMenu, TfiHome, TfiShare, TfiTime, TfiFolder, TfiSettings } from "rea
 import "../../styles/navigation-bar.css";
 
 function Navigation() {
-	// State to manage the collapse state
 	const [isCollapsed, setIsCollapsed] = useState(false);
 
-	// Toggle collapse state
 	const toggleSidebar = () => setIsCollapsed(!isCollapsed);
 
 	return (
 		<div
-			className={`fixed top-0 left-0 h-screen ${
-				isCollapsed ? "w-16" : "w-52"
+			className={`relative top-0 left-0 h-screen ${
+				isCollapsed ? "w-16" : "w-60"
 			} m-0 flex flex-col bg-primary-background text-primary-text text-xl transition-width duration-300 ease-in-out`}
 		>
 			{/* Menu Icon */}
 			<div className="flex flex-row justify-between items-center mt-4 px-4">
-				{!isCollapsed && <span className="font-semibold">FileTransfer</span>}
+				{!isCollapsed && <span className="font-bold text-violet-200">SHARE KO-TO</span>}
 				<TfiMenu className="sidebar-icon cursor-pointer" onClick={toggleSidebar} />
 			</div>
 
 			{/* Links */}
-			<div className="flex flex-col mt-8 space-y-4">
+			<div className="flex flex-col mt-8 ">
 				<NavLink
 					to="/"
-					className={`${isCollapsed ? "sidebar-icons-container-collapsed" : "sidebar-icons-container-expanded"} sidebar-hover-animation`}
+					className={({ isActive }) =>
+						`${isActive ? "active-link" : ""} ${
+							isCollapsed ? "sidebar-icons-container-collapsed" : "sidebar-icons-container-expanded"
+						} sidebar-hover-animation`
+					}
 				>
 					<TfiHome className="sidebar-icon" />
 					{!isCollapsed && <span>Home</span>}
 				</NavLink>
 				<NavLink
 					to="/file-transfer"
-					className={`${isCollapsed ? "sidebar-icons-container-collapsed" : "sidebar-icons-container-expanded"} sidebar-hover-animation`}
+					className={({ isActive }) =>
+						`${isActive ? "active-link" : ""} ${
+							isCollapsed ? "sidebar-icons-container-collapsed" : "sidebar-icons-container-expanded"
+						} sidebar-hover-animation`
+					}
 				>
 					<TfiShare className="sidebar-icon" />
 					{!isCollapsed && <span>File Transfer</span>}
 				</NavLink>
 				<NavLink
 					to="/recent-transfer"
-					className={`${isCollapsed ? "sidebar-icons-container-collapsed" : "sidebar-icons-container-expanded"} sidebar-hover-animation`}
+					className={({ isActive }) =>
+						`${isActive ? "active-link" : ""} ${
+							isCollapsed ? "sidebar-icons-container-collapsed" : "sidebar-icons-container-expanded"
+						} sidebar-hover-animation`
+					}
 				>
 					<TfiTime className="sidebar-icon" />
 					{!isCollapsed && <span>Recent Transfer</span>}
 				</NavLink>
 				<NavLink
 					to="/my-files"
-					className={`${isCollapsed ? "sidebar-icons-container-collapsed" : "sidebar-icons-container-expanded"} sidebar-hover-animation`}
+					className={({ isActive }) =>
+						`${isActive ? "active-link" : ""} ${
+							isCollapsed ? "sidebar-icons-container-collapsed" : "sidebar-icons-container-expanded"
+						} sidebar-hover-animation`
+					}
 				>
 					<TfiFolder className="sidebar-icon" />
 					{!isCollapsed && <span>My Files</span>}
@@ -57,12 +71,14 @@ function Navigation() {
 			{/* Account Settings */}
 			<NavLink
 				to="/account-settings"
-				className={`mt-auto ${
-					isCollapsed ? "sidebar-icons-container-collapsed" : "sidebar-icons-container-expanded"
-				} sidebar-hover-animation`}
+				className={({ isActive }) =>
+					`${isActive ? "active-link" : ""} ${
+						isCollapsed ? "sidebar-icons-container-collapsed" : "sidebar-icons-container-expanded"
+					} sidebar-hover-animation mt-auto`
+				}
 			>
 				<TfiSettings className="sidebar-icon" />
-				{!isCollapsed && <span>Account Settings</span>}
+				{!isCollapsed && <span>My Account</span>}
 			</NavLink>
 		</div>
 	);
