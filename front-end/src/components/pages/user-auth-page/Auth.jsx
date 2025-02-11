4e1e3e50ef452521b52362c27687fdc2b4816f53
vonner04 <randyleean092m@gmail.com>
2025-02-11 19:39:05 +1300

    bug: success notification doesn't appear in other components.

--- front-end/src/components/pages/user-auth-page/Auth.jsx
+++ front-end/src/components/pages/user-auth-page/Auth.jsx
@@ -45,6 +45,49 @@ export default function Auth() {
 			{/* Form */}
 			<div className="bg-primary-background p-8 w-96 rounded-lg shadow-md">
 				<h2 className="text-center text-2xl font-semibold mb-6">{isRegistering ? "Register" : "Login"}</h2>
+
+				<form onSubmit={handleSubmit} className="flex flex-col">
+					<input
+						type="email"
+						name="email"
+						placeholder="Email"
+						value={formData.email}
+						onChange={handleChange}
+						className="p-3 rounded-md bg-gray-200 text-black mb-4 focus:outline-none focus:ring-2 focus:ring-blue-400"
+						required
+					/>
+
+					<input
+						type="password"
+						name="password"
+						placeholder="Password"
+						value={formData.password}
+						onChange={handleChange}
+						className="p-3 rounded-md bg-gray-200 text-black mb-4 focus:outline-none focus:ring-2 focus:ring-blue-400"
+						required
+					/>
+
+					{isRegistering && ( //Show confirm password field only when registering
+						<input
+							type="password"
+							name="confirmPassword"
+							placeholder="Confirm Password"
+							value={formData.confirmPassword}
+							onChange={handleChange}
+							className="p-3 rounded-md bg-gray-200 text-black mb-4 focus:outline-none focus:ring-2 focus:ring-blue-400"
+							required
+						/>
+					)}
+					<button type="submit" className="bg-blue-500 text-white p-3 rounded-md hover:bg-primary-highlight-border">
+						{isRegistering ? "Register" : "Login"}
+					</button>
+				</form>
+				<p className="text-center mt-4">
+					{isRegistering ? "Already have an account?" : "Don't have an account?"}{" "}
+					<span onClick={toggleRegister} className="text-primary-highlight-border cursor-pointer hover:underline">
+						{isRegistering ? "Login" : "Register"}
+					</span>
+				</p>
 			</div>
 		</div>
 	);
